ea7f1bd23615c11bc02ca1a1aaaa1205a5c5efce
ihadd <ihaddou9803@gmail.com>
2020-10-03 17:03:17 -0400

    Added Changes to the Chat amanger class Clear chat is a WIP

--- src/main/java/business_layer/ChatManager.java
+++ src/main/java/business_layer/ChatManager.java
@@ -1,25 +1,143 @@
 package business_layer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
+
+class MessageInfo{
+    private String chat_name;
+    private String message;
+
+    MessageInfo(String msg){
+        chat_name = "Anonymous";
+        message=msg;
+    }
+    MessageInfo(String cn, String msg){
+        chat_name = cn;
+        message=msg;
+    }
+
+
+
+    public String getChat_name() {
+        return chat_name;
+    }
+
+    public void setChat_name(String chat_name) {
+        this.chat_name = chat_name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String toString(){
+        return this.getChat_name() +"," +this.getMessage()+";";
+    }
+}
+
+
+
+
 public class ChatManager {
-    private final TreeMap<Long, List<ChatMessage>> messageStorage;
+    private Long chat_time;
+    private TreeMap<Long, MessageInfo> chatmanager;
 
-    public ChatManager() {
-        this.messageStorage = new TreeMap<>();
+    ChatManager() {
+        chatmanager  = new TreeMap<Long, MessageInfo>();
     }
 
-    public ChatMessage postMessage(String user, String message) {
-        long timestamp = System.currentTimeMillis();
-        ChatMessage chatMessage = new ChatMessage(user, message, timestamp);
-        this.addChatMessage(timestamp, chatMessage);
-        return chatMessage;
+
+    public TreeMap PostMessage(String user, String message) throws InterruptedException {
+
+        if((user=="1")||(user.isEmpty())){
+            user = "Anonymous";
+        }
+        MessageInfo messagenew = new MessageInfo(user, message);
+        chat_time = System.currentTimeMillis();
+        this.chatmanager.put(chat_time, messagenew);
+        Thread.sleep(1);
+
+
+
+        return this.chatmanager;
     }
 
-    private void addChatMessage(long timestamp, ChatMessage chatMessage) {
-        this.messageStorage.putIfAbsent(timestamp, new ArrayList<>());
-        this.messageStorage.get(timestamp).add(chatMessage);
+
+
+    public SortedMap ListMessages(Long x, Long y) throws InterruptedException {
+        SortedMap<Long, MessageInfo> treemapincl = new TreeMap<Long, MessageInfo>();
+        treemapincl = chatmanager.subMap(x,y);
+        return treemapincl;
+
     }
+    //WIP
+    public SortedMap ClearChat(Long x, Long y) throws InterruptedException {
+        SortedMap<Long, MessageInfo> treemapincl = new TreeMap<Long, MessageInfo>();
+        treemapincl = chatmanager.subMap(x,y);
+
+        return treemapincl;
+
+    }
+
+    public long getChat_time() {
+        return chat_time;
+    }
+
+
+    public void setChat_time(long chat_time) {
+        this.chat_time = chat_time;
+    }
+
+    public TreeMap<Long, MessageInfo> getChatmanager() {
+        return chatmanager;
+    }
+
+    public void setChatmanager(TreeMap<Long, MessageInfo> chatmanager) {
+        this.chatmanager = chatmanager;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatManager{chatmanager=" + chatmanager +
+                '}';
+    }
+
+
+
+//Tester doesn't need to be here
+    public static void main(String args[]) throws InterruptedException {
+        ChatManager chatBox = new ChatManager();
+        chatBox.PostMessage("","Hello World");
+
+        chatBox.PostMessage("Bob","Hello Worlod");
+
+        chatBox.PostMessage("Bob","Hello Wosddrld");
+
+
+        chatBox.PostMessage("Bob","Hello Woasdrld");
+
+
+
+        System.out.println(chatBox);
+        //make sure to include L
+        System.out.println(chatBox.ListMessages(0L,1611757506300L));
+
+
+        // creating maps
+
+
+
+
+
+
+    }
+
+
+
+
 }
