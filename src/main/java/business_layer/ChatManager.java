25fbe33b38b285bcec90dcc44252c4cd8063f812
ihadd <ihaddou9803@gmail.com>
2020-10-05 11:21:22 -0400

    Fixed List function returning flat list; Added default check for Anon as per characteristics

--- src/main/java/business_layer/ChatManager.java
+++ src/main/java/business_layer/ChatManager.java
@@ -1,6 +1,7 @@
 package business_layer;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class ChatManager {
@@ -13,7 +14,7 @@ public class ChatManager {
     }
 
 
-    public ArrayList PostMessage(String user, String message) throws InterruptedException {
+    public ArrayList<ChatMessage> PostMessage(String user, String message) throws InterruptedException {
         chatTime = System.currentTimeMillis();
         ChatMessage messagenew;
         if(user==""||user.isEmpty()){
@@ -36,15 +37,14 @@ public class ChatManager {
     }
 
 
-    public ArrayList ListMessages(Long x, Long y) throws InterruptedException {
+    public List<ChatMessage> ListMessages(Long x, Long y) throws InterruptedException {
         SortedMap<Long, ArrayList<ChatMessage>> treemapincl = new TreeMap<Long, ArrayList<ChatMessage>>();
         ArrayList<ArrayList<ChatMessage>> holder = new ArrayList<>();
         treemapincl = this.chatmanager.subMap(x,true, y,true);
-        holder.addAll(treemapincl.values());
-        return holder;
+
+        return treemapincl.values().stream().flatMap(List::stream).collect(Collectors.toList());
 
     }
-
 
 
     @Override
@@ -54,47 +54,5 @@ public class ChatManager {
     }
 
 
-//    //Tester doesn't need to be here
-//    public static void main(String args[]) throws InterruptedException {
-//        ChatManager chatBox = new ChatManager();
-//        chatBox.PostMessage("", "Hello");
-//
-//        chatBox.PostMessage("Bob", "Hello World");
-//        Thread.sleep(1000);
-//
-//        chatBox.PostMessage("Bob", "Hld");
-//        Thread.sleep(1000);
-//
-//        chatBox.PostMessage("", "Hello");
-//
-//        System.out.print(chatBox.PostMessage("Bob", "Hello World"));
-//
-//
-//
-//
-//        System.out.println(chatBox);
-//
-//
-////        //make sure to include L
-//       System.out.println(chatBox.ListMessages(1601852620961L, 1601852720961L));
-//       System.out.println(chatBox.ClearMessages());
-////
-////        System.out.println(chatBox.ClearMessages(1601846232701L,1601846482281L));
-////        System.out.println(chatBox.ClearMessages());
-//
-//
-//
-//
-//        // creating maps
-//
-//
-//    }
-
 }
-
-
-
-
-
-
 
