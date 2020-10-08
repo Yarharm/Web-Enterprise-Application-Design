1f4d75f4d3a4ae79f1e55a6ba371a12536887366
yarharm <yarharm@gmail.com>
2020-10-07 23:29:18 -0400

    Add warning on empty message #23

--- src/main/java/helpers/SharedConstants.java
+++ src/main/java/helpers/SharedConstants.java
@@ -2,4 +2,5 @@ package helpers;
 
 public final class SharedConstants {
     public static final String CHAT_PAGE = "index.jsp";
+    public static final String DISPLAY_WARNING_POPUP = "displayWarningPopup";
 }
