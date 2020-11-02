55334229fbde394cb711128e74464baf72f3e5cc
yarharm <yarharm@gmail.com>
2020-11-02 16:26:59 -0500

    Move config file to the WEB-INf folder

--- MessageBoardSystem/src/main/java/configuration/ConfigDriver.java
+++ MessageBoardSystem/src/main/java/configuration/ConfigDriver.java
@@ -1,20 +1,21 @@
 package configuration;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public final class ConfigDriver {
-    private final static String MODULE_NAME = "MessageBoardSystem";
     private final static String CONFIG_NAME = "config.json";
-    private final static String CONFIG_PATH = Paths.get(System.getProperty("user.dir"), MODULE_NAME, CONFIG_NAME).toString();
     private final static Gson gson = new Gson();
     private ConfigDriver() {}
 
@@ -41,13 +42,26 @@ public final class ConfigDriver {
     private static Map<String, String> getConfig() {
         Map<String, String> configMap = new HashMap<>();
         try {
-            JsonReader jsonConfigFile = new JsonReader(new FileReader(CONFIG_PATH));
+            String configPath = buildConfigPath();
+            JsonReader jsonConfigFile = new JsonReader(new FileReader(configPath));
             Type type = new TypeToken<Map<String, String>>(){}.getType();
             configMap = gson.fromJson(jsonConfigFile, type);
-        } catch(FileNotFoundException e) {
+        } catch(IOException e) {
             System.err.println("Missing Configuration file.");
             System.exit(1);
         }
         return configMap;
+    }
+
+    private static String buildConfigPath() throws IOException {
+        Path rootPath = Paths.get(System.getProperty("user.dir")).toAbsolutePath().normalize();
+        if(rootPath.toString().contains("tomcat")) {
+            rootPath = rootPath.getParent();
+        }
+        Optional<Path> hit = Files.walk(rootPath)
+                .filter(file -> file.getFileName().toString().equals(CONFIG_NAME))
+                .findAny();
+
+        return hit.get().toString();
     }
 }
