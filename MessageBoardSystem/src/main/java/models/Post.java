ef976b37143bc89ec3eb25303a4170003480cc8b
yarharm <yarharm@gmail.com>
2020-11-06 19:08:53 -0500

    Add attachment upload #57

--- MessageBoardSystem/src/main/java/models/Post.java
+++ MessageBoardSystem/src/main/java/models/Post.java
@@ -1,5 +1,8 @@
 package models;
 
+import helpers.Utils;
+
+import java.io.InputStream;
 import java.io.Serializable;
 
 public class Post implements Serializable {
@@ -8,6 +11,8 @@ public class Post implements Serializable {
     private String postTitle;
     private String message;
     private long timestamp;
+    private String attachment;
+    private boolean containsAttachment;
 
     public Post() {}
     public Post(int userID, String postTitle, String message, long timestamp) {
@@ -15,6 +20,30 @@ public class Post implements Serializable {
         this.postTitle = postTitle;
         this.message = message;
         this.timestamp = timestamp;
+        this.containsAttachment = false;
+    }
+
+    public void setAttachmentFromBinary(InputStream stream) {
+        if(stream != null) {
+            this.containsAttachment = true;
+            this.attachment = Utils.inputStreamToImage(stream);
+        }
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public boolean isContainsAttachment() {
+        return containsAttachment;
+    }
+
+    public void setContainsAttachment(boolean containsAttachment) {
+        this.containsAttachment = containsAttachment;
     }
 
     public int getUserID() {
--- MessageBoardSystem/src/main/java/servlet/BasicServlet.java
+++ MessageBoardSystem/src/main/java/servlet/BasicServlet.java
@@ -5,25 +5,35 @@ import helpers.FrontendBoardManager;
 import models.Post;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 
 import static helpers.Constants.ROOT_PAGE;
 
 @WebServlet(name = "BasicServlet")
+@MultipartConfig
 public class BasicServlet extends HttpServlet {
     MessageBoardManager boardManager = new MessageBoardManager();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String message = request.getParameter("message");
         String title = request.getParameter("title");
+        Part attachmentPart = request.getPart("attachment");
         int userID = (int) request.getSession().getAttribute("userID");
 
         if(message != null && title != null && !message.isEmpty() && !title.isEmpty()) {
             Post post = boardManager.postMessage(userID, title, message);
+
+            if(attachmentPart.getSize() > 0) {
+                this.boardManager.saveAttachment(post, attachmentPart.getSubmittedFileName(), attachmentPart.getSize(),
+                        attachmentPart.getContentType(), attachmentPart.getInputStream());
+
+            }
             FrontendBoardManager.appendMessageBoard(request, post);
         }
 
