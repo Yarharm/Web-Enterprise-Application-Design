ea7cfd7e9abb11c380f5a78810a993d70e6239c9
yarharm <yarharm@gmail.com>
2020-11-07 16:30:11 -0500

    Delete post #49

---
+++ MessageBoardSystem/src/main/java/servlet/PostDeleteServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import business_layer.MessageBoardManager;
+import helpers.FrontendBoardManager;
+import models.Post;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static helpers.Constants.DISPLAY_WARNING_POPUP;
+import static helpers.Constants.ROOT_PAGE;
+
+@WebServlet(name = "PostDeleteServlet")
+public class PostDeleteServlet extends HttpServlet {
+    MessageBoardManager boardManager = new MessageBoardManager();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String postID = request.getParameter("postID");
+        try {
+            if(postID == null || postID.isEmpty()) {
+                throw new Exception("Delete failed! Missing postID");
+            }
+            boolean deleteStatus = boardManager.deletePost(Integer.parseInt(postID));
+            if(!deleteStatus) {
+                throw new Exception("Delete failed! Post does not exist");
+            } else {
+                List<Post> freshPosts = boardManager.getAllPosts();
+                FrontendBoardManager.refreshMessageBoard(request, freshPosts);
+            }
+
+        } catch (Exception e) {
+            request.getServletContext().setAttribute(DISPLAY_WARNING_POPUP, e.getMessage());
+        } finally {
+            response.sendRedirect(ROOT_PAGE);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- MessageBoardSystem/src/main/webapp/updatePost.jsp
+++ MessageBoardSystem/src/main/webapp/updatePost.jsp
@@ -12,9 +12,6 @@
     <title>Title</title>
 </head>
 <body>
-<c:if test="${sessionScope.userID == null}">
-    <c:redirect url="index.jsp"/>
-</c:if>
 
 <form method="post" action="servlet.UpdateServlet">
 
