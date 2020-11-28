aca9ab1b427d3c8ffa6179f3a8a57678561e80d5
ihadd <ihaddou9803@gmail.com>
2020-11-28 11:27:52 -0500

    added successful user file

--- MessageBoardSystem/src/main/java/business_layer/UserManager.java
+++ MessageBoardSystem/src/main/java/business_layer/UserManager.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 public class UserManager {
     private final static String USER_FILE_NAME = "users.json";
+    private final static String USER_NAME = null;
+
 
     public Set<String> loadGroupMembership() {
-        return this.loadGroupMembership(USER_FILE_NAME);
+        return this.loadGroupMembership(USER_FILE_NAME,USER_NAME);
     }
 
-    public Set<String> loadGroupMembership(String userConfig) {
+    public Set<String> loadGroupMembership(String userConfig, String userName) {
         return null;
     }
 }
--- MessageBoardSystem/src/test/java/UserManagerTest.java
+++ MessageBoardSystem/src/test/java/UserManagerTest.java
@@ -6,6 +6,9 @@ public class UserManagerTest {
     private static final String CIRCULAR_DEPENDENCY_DIFFERENT_GROUP_TEST_DATA = "circularDependencyDifferentGroup.json";
     private static final String CIRCULAR_DEPENDENCY_SAME_GROUP_DATA = "circularDependencySameGroup.json";
     private static final String MISSING_PARENT_GROUP_DATA = "missingParentData.json";
+    private static final String SUCCESSFUL_USER_DATA = "successfulUser.json";
+    private static final String USER_NAME = null;
+
 
     UserManager userManager = null;
 
@@ -16,16 +19,19 @@ public class UserManagerTest {
 
     @Test(expected = Exception.class)
     public void testCircularDependencyDifferentGroup() {
-        userManager.loadGroupMembership(CIRCULAR_DEPENDENCY_DIFFERENT_GROUP_TEST_DATA);
+        userManager.loadGroupMembership(CIRCULAR_DEPENDENCY_DIFFERENT_GROUP_TEST_DATA,USER_NAME);
     }
-
+    @Test
+    public void testSuccessfulUser() {
+        userManager.loadGroupMembership(SUCCESSFUL_USER_DATA,USER_NAME);
+    }
     @Test(expected = Exception.class)
     public void testCircularDependencySameGroup() {
-        userManager.loadGroupMembership(CIRCULAR_DEPENDENCY_SAME_GROUP_DATA);
+        userManager.loadGroupMembership(CIRCULAR_DEPENDENCY_SAME_GROUP_DATA,USER_NAME);
     }
 
     @Test(expected = Exception.class)
     public void testMissingParentGroup() {
-        userManager.loadGroupMembership(MISSING_PARENT_GROUP_DATA);
+        userManager.loadGroupMembership(MISSING_PARENT_GROUP_DATA,USER_NAME);
     }
 }
