5064f0c3e20e33ec941e07573398fadbc81c8c9a
ihadd <ihaddou9803@gmail.com>
2020-12-06 20:55:58 -0500

    Admin edit and delete working

--- MessageBoardSystem/src/main/java/servlet/UpdateServlet.java
+++ MessageBoardSystem/src/main/java/servlet/UpdateServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.util.Set;
 
 import static helpers.Constants.*;
 
@@ -28,6 +29,7 @@ public class UpdateServlet extends HttpServlet {
         Part attachmentPart = request.getPart("attachment");
         Object postAttribute = request.getSession().getAttribute("referredPost");
 
+
         try {
             if(title == null || title.isEmpty() || message == null || message.isEmpty() || postAttribute == null) {
                 throw new Exception("Edit failed! Invalid attributes");
@@ -35,7 +37,8 @@ public class UpdateServlet extends HttpServlet {
             
             int userID = (int) request.getSession().getAttribute("userID");
             Post post = (Post) postAttribute;
-            if(!messageBoardManager.isPostOwner(userID, post.getPostID()) && request.isUserInRole("admins")) {
+            Set<String> groupMemberships = (Set<String>) request.getSession().getAttribute(GROUP_MEMBERSHIP_SESSION_ATTRIBUTE);
+            if(!messageBoardManager.isPostOwner(userID, post.getPostID()) && !groupMemberships.contains("admins") ) {
                 throw new Exception("Edit failed! Permission denied");
             }
 
@@ -78,7 +81,8 @@ public class UpdateServlet extends HttpServlet {
 
             int postID = Integer.parseInt(postIDParam);
             int userID = (int) request.getSession().getAttribute("userID");
-            if(!messageBoardManager.isPostOwner(userID, postID) && request.isUserInRole("admins")) {
+            Set<String> groupMemberships = (Set<String>) request.getSession().getAttribute(GROUP_MEMBERSHIP_SESSION_ATTRIBUTE);
+            if(!messageBoardManager.isPostOwner(userID, postID) && !groupMemberships.contains("admins") ) {
                 throw new Exception("Edit failed! Permission denied");
             }
 
